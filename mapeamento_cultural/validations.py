ffb4562cde177a8ec47ebbd9133c9163a79968b7
ViniciusSoldati <visolmattos10@gmail.com>
2023-07-25 17:55:40 -0300

    24.07.23.1.VS
    Continuando resposividade do sistema, refazendo páginas com outras dimensões

--- mapeamento_cultural/validations.py
+++ mapeamento_cultural/validations.py
@@ -3,7 +3,7 @@ from itertools import cycle
 
 from django.core.validators import EMPTY_VALUES
 from django.forms import ValidationError
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 error_messages = {
     'invalid_CNPJ': _("CNPJ inválido."),    
